ea31b938ecba057bfec7b2b3d9478f2864181703
DaniBonica001 <danielabc_0106@hotmail.com>
2024-03-26 17:30:34 -0500

    mongo model

---
+++ compute/src/infrastructure/database/mongo-users.database.ts
@@ -0,0 +1,14 @@
+import { User as UserDomain } from '@/domain/model/User'
+import mongoose, { Model } from 'mongoose'
+const { Schema, model } = mongoose
+
+const UserSchema = new Schema<UserDomain>({
+  name: String,
+  email: {type: String, unique: true},
+  password: String,
+  role: String
+})
+
+const User: Model<UserDomain> = mongoose.models.User|| model('User', UserSchema)
+
+export default User
